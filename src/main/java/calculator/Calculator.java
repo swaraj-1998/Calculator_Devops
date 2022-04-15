fd4a90fd00be0f98ea3ffdb0e8cf8474edab9486
swaraj-1998 <swarajwednesday@gmail.com>
2022-04-15 17:47:44 +0530

    Commit18

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,63 +6,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Calculator {
-
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
-
-    public Calculator() {
-    }
-
-    public double getSquareRoot(double num) {
-        logger.info("[SQUARE ROOT] - " + num);
-        double ans = Math.sqrt(num);
-        logger.info("[ANS - SQUARE ROOT] - " + ans);
-        return ans;
-    }
-    public double fact(double num) {
-        double facto = 1;
-        for(int i = 1; i <= num; ++i)
-        { facto *= i;   }
-        return  facto;
-    }
-    public double getFactoral(double num) {
-        logger.info("[FACTORIAL] - " + num);
-        double result = fact(num);
-        logger.info("[RESULT - FACTORIAL] - " + result);
-        return result;
-    }
-
-    public double getPower(double num1, double num2) {
-        logger.info("[POWER - " + num1 + " RAISED TO] " + num2);
-        double result = Math.pow(num1,num2);
-        logger.info("[RESULT - POWER] - " + result);
-        return result;
-    }
-
-    public double getNaturalLog(double number1) {
-        logger.info("[NATURAL LOG] - " + number1);
-        double result = 0;
-        try {
-
-            if (number1 <0 ) {
-                result = Double.NaN;
-                throw new ArithmeticException("Case of NaN 0.0/0.0");
-            }
-            else {
-                result = Math.log(number1);
-            }
-        } catch (ArithmeticException error) {
-            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
-        }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
-        return result;
-    }
     public static void main(String[] args) {
 
         Calculator calc = new Calculator();
         Scanner scanner = new Scanner(System.in);
         double num1, num2;
         do {
-            System.out.println("Calculator : ");
+            System.out.println("Calculator Using Devops : ");
             System.out.print("1 : Factorial\n2 : Square Root\n3 : Power\n4 : Natural Logarithm\n" +
                     "5 : Exit\nChoose Your Choice : ");
             int ch;
@@ -76,7 +26,7 @@ public class Calculator {
                     // do factorial
                     System.out.print("Enter Number : ");
                     num1 = scanner.nextDouble();
-                    System.out.println("Factorial of "+num1+" is : " + calc.getFactoral(num1));
+                    System.out.println("Factorial is : " + calc.getFactoral(num1));
                     System.out.println("\n");
 
                     break;
@@ -84,7 +34,7 @@ public class Calculator {
                     // find square root
                     System.out.print("Enter a number : ");
                     num1 = scanner.nextDouble();
-                    System.out.println("Square root of "+num1+" is : " + calc.getSquareRoot(num1));
+                    System.out.println("Square root is : " + calc.getSquareRoot(num1));
                     System.out.println("\n");
 
 
@@ -95,21 +45,71 @@ public class Calculator {
                     num1 = scanner.nextDouble();
                     System.out.print("Enter the second number : ");
                     num2 = scanner.nextDouble();
-                    System.out.println(num1+ " raised to power "+num2+" is : " + calc.getPower(num1, num2));
+                    System.out.println("Power is : " + calc.getPower(num1, num2));
                     System.out.println("\n");
                     break;
                 case 4:
                     // find natural log
                     System.out.print("Enter a number : ");
                     num1 = scanner.nextDouble();
-                    System.out.println("Natural log of "+num1+" is : " + calc.getNaturalLog(num1));
+                    System.out.println("Natural log is :" + calc.getNaturalLog(num1));
                     System.out.println("\n");
 
                     break;
                 default:
-                    System.out.println("Exiting......");
+                    System.out.println("Turning Off......");
                     return;
             }
         } while (true);
     }
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+
+    public Calculator() {
+    }
+
+    public double getSquareRoot(double num) {
+        logger.info("[SQUARE ROOT OF] - " + num);
+        double ans = Math.sqrt(num);
+        logger.info("[ANS - SQUARE ROOT] - " + ans);
+        return ans;
+    }
+    public double fact(double num) {
+        double facto = 1;
+        for(int i = 1; i <= num; ++i)
+        { facto *= i;   }
+        return  facto;
+    }
+    public double getFactoral(double num) {
+        logger.info("[FACTORIAL OF] - " + num);
+        double result = fact(num);
+        logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+
+    public double getPower(double num1, double num2) {
+        logger.info("[POWER - " + num1 + " RAISED TO] " + num2);
+        double result = Math.pow(num1,num2);
+        logger.info("[RESULT - POWER] - " + result);
+        return result;
+    }
+
+    public double getNaturalLog(double number1) {
+        logger.info("[NATURAL LOG OF] - " + number1);
+        double result = 0;
+        try {
+
+            if (number1 <0 ) {
+                result = Double.NaN;
+                throw new ArithmeticException("Exception Occurred");
+            }
+            else {
+                result = Math.log(number1);
+            }
+        } catch (ArithmeticException error) {
+            System.out.println("Exception Occurred");
+        }
+        logger.info("[RESULT - NATURAL LOG] - " + result);
+        return result;
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,55 +3,47 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculatorTest {
-    private static final double DELTA = 1e-15;
+    private static final double DELTA = 1e-10;
     Calculator calculator = new Calculator();
 
     @Test
-    public void factorialTruePositive(){
-        assertEquals("Finding factorial of a number for True Positive", 6, calculator.fact(3), DELTA);
-        assertEquals("Finding factorial of a number for True Positive", 24, calculator.fact(4), DELTA);
+    public void factorial1(){
+        assertEquals("Factorial Positive", 24, calculator.fact(4), DELTA);
     }
 
     @Test
-    public void factorialFalsePositive(){
-        assertNotEquals("Finding factorial of a number for False Positive", 120, calculator.fact(6), DELTA);
-        assertNotEquals("Finding factorial of a number for False Positive", 24, calculator.fact(3), DELTA);
+    public void factorial2(){
+        assertNotEquals("Factorial False Positive", 120, calculator.fact(6), DELTA);
     }
 
     @Test
-    public void powerTruePositive(){
-        assertEquals("Finding power for True Positive", 8, calculator.getPower(2, 3), DELTA);
-        assertEquals("Finding power for True Positive", 64, calculator.getPower(4, 3), DELTA);
+    public void power1(){
+        assertEquals("Finding power", 8, calculator.getPower(2, 3), DELTA);
     }
 
     @Test
-    public void powerFalsePositive(){
-        assertNotEquals("Finding power for False Positive", 6, calculator.getPower(2, 2), DELTA);
-        assertNotEquals("Finding power for False Positive", -7.3, calculator.getPower(2, 3), DELTA);
+    public void power2(){
+        assertNotEquals("Finding power", 6, calculator.getPower(2, 2), DELTA);
     }
 
     @Test
-    public void logTruePositive(){
-        assertEquals("Finding natural log for True Positive", 0, calculator.getNaturalLog(1), DELTA);
-        assertEquals("Finding natural log for True Positive", 0, calculator.getNaturalLog(1), DELTA);
+    public void log1(){
+        assertEquals("Finding natural log", 0, calculator.getNaturalLog(1), DELTA);
     }
 
     @Test
-    public void logFalsePositive(){
-        assertNotEquals("Finding natural log for False Positive", 6, calculator.getNaturalLog(2.4), DELTA);
-        assertNotEquals("Finding natural log for False Positive", 7.3, calculator.getNaturalLog(2.1), DELTA);
+    public void log2(){
+        assertNotEquals("Finding natural log false", 6, calculator.getNaturalLog(2.4), DELTA);
     }
 
     @Test
-    public void sqrootTruePositive(){
-        assertEquals("Finding square root for True Positive", 2, calculator.getSquareRoot(4), DELTA);
-        assertEquals("Finding square root for True Positive", 1, calculator.getSquareRoot(1), DELTA);
+    public void sqroot1(){
+        assertEquals("Finding square root true", 2, calculator.getSquareRoot(4), DELTA);
     }
 
     @Test
-    public void sqrootFalsePositive(){
-        assertNotEquals("Finding square root for False Positive", 1, calculator.getSquareRoot(3), DELTA);
-        assertNotEquals("Finding square root for False Positive", 0, calculator.getSquareRoot(4), DELTA);
+    public void sqroot2(){
+        assertNotEquals("Finding square root false", 1, calculator.getSquareRoot(3), DELTA);
 
     }
 
